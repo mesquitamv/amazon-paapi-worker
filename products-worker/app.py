de6723198279fa72b89c0462aae4ce07eac82a49
Marcus Mesquita <mesquita.marcusv@gmail.com>
2023-12-15 09:41:05 -0300

    Starting MongoDB configuration

--- products-worker/app.py
+++ products-worker/app.py
@@ -3,6 +3,11 @@ from .routes.routes import routes_bp
 
 app = Flask(__name__)
 
+app.config ['MONGODB_SETTINGS'] = {
+    'db': '',
+    'host': '',
+    'port': ''
+}
 app.register_blueprint(routes_bp)
 
 if __name__ == "__main__":
